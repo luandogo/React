4bea4bb439678765e884455add18e66d5c1c5d47
Antonio Domínguez <luandogo97@gmail.com>
2021-12-13 21:52:20 -0600

    Add GifApp

---
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=aK0BP40YEMwl7t1tSyYvCZkj7g3qoxAI`;
+    const resp =  await fetch( url );
+    const {data} = await resp.json();
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    return gifs;
+}
